02fd138dd245ce13f5da19fde437533be4dec85e
Mohammed El Makrini <81011948+Medmakrini@users.noreply.github.com>
2023-02-02 22:26:25 +0100

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
 import App from './App';
-//import { expect } from 'jest';
+import { expect } from 'jest';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });
